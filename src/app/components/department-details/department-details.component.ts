71e57fe2f2859258c387d44b9cc9690aee1185b3
adenita <nittaluta@gmail.com>
2023-10-06 01:52:54 +0200

    added department details component and added reference to program list component

---
+++ src/app/components/department-details/department-details.component.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {DepartmentService} from "../../core/http/department.service";
+import {BehaviorSubject} from "rxjs";
+import { DepartmentTransport} from "../../shared/models/department";
+import {Professor} from "../../shared/models/professor";
+import {Classroom} from "../../shared/models/classroom";
+import {Program, ProgramDetails} from "../../shared/models/program";
+
+@Component({
+  selector: 'app-department-details',
+  templateUrl: './department-details.component.html',
+  styleUrls: ['./department-details.component.css']
+})
+export class DepartmentDetailsComponent implements OnInit {
+  departmentId: number = -1;
+  department: DepartmentTransport = {} as DepartmentTransport;
+  previewPrograms: BehaviorSubject<Program[] | ProgramDetails[]>;
+
+  constructor(private route: ActivatedRoute, private departmentService: DepartmentService) {
+    this.previewPrograms = new BehaviorSubject<Program[] | ProgramDetails[]>([]);
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.departmentId = +params['id'];
+      this.getDepartment(this.departmentId);
+    });
+  }
+
+   getDepartment(departmentId: number) {
+    this.departmentService.getDepartmentDetails(departmentId).subscribe({next: (department) => {
+       this.department = department;
+       this.previewPrograms.next(department.programTransports.slice(0, 3));
+      }})
+  }
+
+}
