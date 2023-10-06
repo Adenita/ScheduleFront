29846d27e64188654e7f4f6cc812b99f41d0a0ee
adenita <nittaluta@gmail.com>
2023-10-06 02:18:02 +0200

    removed unused import

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,7 +1,7 @@
-import {Injectable, Input} from '@angular/core';
+import {Injectable} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {RouteParametersService} from "./route-parameters.service";
+
 @Injectable({
   providedIn: 'root',
 })
