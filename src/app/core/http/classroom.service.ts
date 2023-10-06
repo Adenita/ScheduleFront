7e4b939ff4e4f9baf6eb60ba80c906c7ebf83e20
adenita <nittaluta@gmail.com>
2023-10-06 02:33:41 +0200

    added http client service for model

---
+++ src/app/core/http/classroom.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Classroom, ClassroomTransport} from "../../shared/models/classroom";
+import {DataService} from "../services/data.service";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ClassroomService extends DataService<Classroom, ClassroomTransport>{
+  constructor(private httpClient: HttpClient) {
+    super(httpClient);
+    this.apiUrl = "classrooms";
+  }
+
+  getLabClassrooms(): Observable<ClassroomTransport> {
+    return this.httpClient.get<ClassroomTransport>(`${this.url}/${this.apiUrl}/lab`);
+  }
+
+  getLectureClassrooms(): Observable<ClassroomTransport> {
+    return this.httpClient.get<ClassroomTransport>(`${this.url}/${this.apiUrl}/lecture`);
+  }
+
+}
