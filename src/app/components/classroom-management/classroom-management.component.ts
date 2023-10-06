3cce207b5a83bf9e720ea1b43f64cd02c17304fa
adenita <nittaluta@gmail.com>
2023-10-06 02:34:23 +0200

    added classroom-list and classroom-management component

---
+++ src/app/components/classroom-management/classroom-management.component.ts
@@ -0,0 +1,65 @@
+import {Component, OnInit} from '@angular/core';
+import {Classroom} from "../../shared/models/classroom";
+import {ClassroomService} from "../../core/http/classroom.service";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {BehaviorSubject} from "rxjs";
+
+@Component({
+  selector: 'app-classroom-management',
+  templateUrl: './classroom-management.component.html',
+  styleUrls: ['./classroom-management.component.css']
+})
+export class ClassroomManagementComponent implements OnInit{
+  classrooms$: BehaviorSubject<Classroom[]>;
+  classroomForm: FormGroup;
+  showForm = false;
+
+  constructor(
+    private classroomService: ClassroomService,
+    private formBuilder: FormBuilder
+  ) {
+    this.classrooms$ = new BehaviorSubject<Classroom[]>([]);
+    this.classroomForm = this.buildFormGroup(formBuilder);
+  }
+
+  ngOnInit() {
+    this.getClassrooms();
+  }
+
+  buildFormGroup(formBuilder: FormBuilder): FormGroup{
+    return formBuilder.group({
+      name: new FormControl('', Validators.required),
+      numberOfSeats: new FormControl(Validators.required, Validators.min(10)),
+      hasComputers: new FormControl(false, Validators.required)
+    })
+  }
+
+  getClassrooms(): void {
+    this.classroomService.getAll().subscribe({
+      next: classroomTransport =>  this.classrooms$.next(classroomTransport.classrooms),
+      error: (err) => console.error('Error fetching classrooms', err)
+    })
+  };
+
+  postClassroom(){
+    if (this.classroomForm.valid) {
+      const classroom = this.classroomForm.value;
+      this.classroomService.post(classroom).subscribe({
+        next: () => {
+          this.getClassrooms();
+          this.closeForm();
+        },
+        error: (err) =>  console.error('Error posting classroom:', err)
+      })
+    }
+  }
+
+  openForm() {
+    this.showForm = true;
+  }
+
+  closeForm() {
+    this.showForm = false;
+  }
+
+}
