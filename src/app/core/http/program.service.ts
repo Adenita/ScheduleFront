d3c94b440b405dc088508f5b78cf7cde104a96fd
adenita <nittaluta@gmail.com>
2023-10-08 07:33:55 +0200

    added new requests regarding student groups to program service

--- src/app/core/http/program.service.ts
+++ src/app/core/http/program.service.ts
@@ -4,6 +4,7 @@ import { DataService } from '../services/data.service';
 import { ProgramDetailsTransport, ProgramListTransport, ProgramTransport } from '../../shared/models/program';
 import { Observable } from 'rxjs';
 import { SubjectListTransport, SubjectTransport } from '../../shared/models/subject';
+import { StudentGroupListTransport, StudentGroupTransport } from '../../shared/models/student-group';
 
 @Injectable({
   providedIn: 'root',
@@ -21,8 +22,16 @@ export class ProgramService extends DataService<ProgramTransport, ProgramListTra
     return this.httpClient.get<SubjectListTransport>(`${this.url}/${this.apiUrl}/${programId}/subjects`);
   }
 
-  addSubjectToProgram(programId: number, subjectTransport: SubjectTransport) {
+  postSubjectToProgram(programId: number, subjectTransport: SubjectTransport) {
     return this.httpClient.post<SubjectTransport>(`${this.url}/${this.apiUrl}/${programId}/subjects`, subjectTransport);
+  }
+
+  getStudentGroupsPerProgram(programId: number): Observable<StudentGroupListTransport> {
+    return this.httpClient.get<StudentGroupListTransport>(`${this.url}/${this.apiUrl}/${programId}/student_groups`);
+  }
+
+  postStudentGroupToProgram(programId: number, studentGroupTransport: StudentGroupTransport) {
+    return this.httpClient.post<StudentGroupTransport>(`${this.url}/${this.apiUrl}/${programId}/student_groups`, studentGroupTransport);
   }
 
   getProgramDetails(programId: number): Observable<ProgramDetailsTransport> {
