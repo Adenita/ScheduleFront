a1cea8c71b255a46036f6852e9ea01d85840060a
adenita <nittaluta@gmail.com>
2023-10-08 05:27:04 +0200

    added all the other route parameters to the service and modified getRouteParams method

--- src/app/core/services/route-parameters.service.ts
+++ src/app/core/services/route-parameters.service.ts
@@ -7,20 +7,39 @@ import { ActivatedRoute } from '@angular/router';
 export class RouteParametersService {
   private _departmentId: number | null = null;
   private _programId: number | null = null;
+  private _professorId: number | null = null;
+  private _subjectId: number | null = null;
+  private _studentGroupId: number | null = null;
 
-  getRouteParams(route: ActivatedRoute) {
-    route.params.subscribe(params => {
-      this._departmentId = params['id'] ? +params['id'] : null;
-      this._programId = params['pid'] ? +params['pid'] : null;
+  getRouteParams(activatedRoute: ActivatedRoute): Promise<void> {
+    return new Promise<void>((resolve) => {
+      activatedRoute.params.subscribe((params) => {
+        this._departmentId = +params['id'] || null;
+        this._programId = +params['pid'] || null;
+        this._professorId = +params['ppid'] || null;
+        this._subjectId = +params['sid'] || null;
+        this._studentGroupId = +params['ssid'] || null;
+        resolve();
+      });
     });
   }
 
-
-  get departmentId(): number  {
+  get departmentId(): number {
     return this._departmentId ?? -1;
   }
 
   get programId(): number {
     return this._programId ?? -1;
+  }
+
+  get professorId(): number {
+    return this._professorId ?? -1;
+  }
+  get subjectId(): number {
+    return this._subjectId ?? -1;
+  }
+
+  get studentGroupId(): number {
+    return this._studentGroupId ?? -1;
   }
 }
