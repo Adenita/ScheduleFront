4abaf2ada463edf99a40fd66df6e994cab4dbddf
adenita <nittaluta@gmail.com>
2023-10-08 07:33:00 +0200

    added http client service for student group

---
+++ src/app/core/http/student-groups.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DataService } from '../services/data.service';
+import { StudentGroupTransport, StudentGroupListTransport } from '../../shared/models/student-group';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StudentGroupsService extends DataService<StudentGroupTransport, StudentGroupListTransport> {
+  constructor(httpClient: HttpClient) {
+    super(httpClient);
+    this.apiUrl = 'student_groups';
+  }
+}
