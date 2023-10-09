75e029dae82bc03ef52a5689bff1772e5fa2adb4
adenita <nittaluta@gmail.com>
2023-10-09 03:35:42 +0200

    updated app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,20 +2,16 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    }),
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'SchdeuleFront'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('SchdeuleFront');
   });
 
   it('should render title', () => {
