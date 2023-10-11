5a645eb40f3c999c3e5c2626156229977815a125
adenita <nittaluta@gmail.com>
2023-10-11 23:29:15 +0200

    added schedule model

---
+++ src/app/shared/models/schedule.ts
@@ -0,0 +1,51 @@
+import { EventTransport } from './event';
+
+export interface ScheduleTransport {
+  events: EventTransport[];
+  fitness: number;
+}
+
+export class Schedule {
+  private _id: number = 0;
+  private _events: EventTransport[] = [];
+  private _fitness: number = -1;
+  private _conflicts: ConflictType[] = [];
+
+  get id(): number {
+    return this._id;
+  }
+
+  set id(value: number) {
+    this._id = value;
+  }
+
+  set fitness(fitness: number) {
+    this._fitness = fitness;
+  }
+
+  get fitness() {
+    return this._fitness;
+  }
+
+  set events(events: EventTransport[]) {
+    this._events = events;
+  }
+
+  get events() {
+    return this._events;
+  }
+
+  get conflicts(): ConflictType[] {
+    return this._conflicts;
+  }
+
+  set conflicts(value: ConflictType[]) {
+    this._conflicts = value;
+  }
+}
+
+export type ConflictType = {
+  event: EventTransport;
+  nextEvent: EventTransport;
+  message: string;
+};
