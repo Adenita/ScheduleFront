bc2ce36ad0be2309eba132927a41bab95c365f73
adenita <nittaluta@gmail.com>
2023-10-11 23:29:34 +0200

    added population class

---
+++ src/app/shared/models/population.ts
@@ -0,0 +1,13 @@
+import { Schedule } from './schedule';
+
+export class Population {
+  private _schedules: Schedule[] = [];
+
+  set schedules(schedules: Schedule[]) {
+    this._schedules = schedules;
+  }
+
+  get schedules() {
+    return this._schedules;
+  }
+}
