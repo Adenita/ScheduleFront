a619f847b0c292ef5d51e21c6a144606feca4a08
adenita <nittaluta@gmail.com>
2023-10-16 05:05:34 +0200

    added http client service for schedule

---
+++ src/app/core/http/schedule-data.service.ts
@@ -0,0 +1,14 @@
+import { DataService } from '../services/data.service';
+import { ScheduleListTransport, ScheduleTransport } from '../../shared/models/schedule';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ScheduleDataService extends DataService<ScheduleTransport, ScheduleListTransport> {
+  constructor(private httpClient: HttpClient) {
+    super(httpClient);
+    this.apiUrl = 'schedules';
+  }
+}
