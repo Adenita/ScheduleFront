1d6133c045f62b7054aebb0e7a412ee918dd114d
adenita <nittaluta@gmail.com>
2023-10-22 01:35:54 +0200

    created professor routing module

---
+++ src/app/department/sub-modules/professor/professor-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProfessorManagementComponent } from './pages/professor-management/professor-management.component';
+import { ProfessorDetailsComponent } from './pages/professor-details/professor-details.component';
+
+const routes: Routes = [
+  {
+    path: 'departments/:id/professors',
+    component: ProfessorManagementComponent,
+  },
+  {
+    path: 'departments/:id/professors/:ppid',
+    component: ProfessorDetailsComponent,
+  },
+  {
+    path: 'departments/:id/programs/:pid/professors',
+    component: ProfessorManagementComponent,
+  },
+  {
+    path: 'departments/:id/programs/:pid/professors/:ppid',
+    component: ProfessorDetailsComponent,
+  },
+  {
+    path: 'departments/:id/subjects/:sid/professors',
+    component: ProfessorManagementComponent,
+  },
+  {
+    path: 'departments/:id/subjects/:sid/professors/:ppid',
+    component: ProfessorDetailsComponent,
+  },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ProfessorRoutingModule {}
