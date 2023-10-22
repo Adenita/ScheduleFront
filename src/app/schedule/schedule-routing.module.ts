e26a80e56ee5c324517907b32dae0c7b23739084
adenita <nittaluta@gmail.com>
2023-10-22 02:34:19 +0200

    created schedule routing module

---
+++ src/app/schedule/schedule-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ScheduleManagementComponent } from './pages/schedule-management/schedule-management.component';
+
+const routes: Routes = [{ path: 'schedules', component: ScheduleManagementComponent }];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ScheduleRoutingModule {}
