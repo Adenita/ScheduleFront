f202a925bcb561b5a697d58e7850b5b6700a63cd
adenita <nittaluta@gmail.com>
2023-10-22 02:09:38 +0200

    updated classroom and app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ClassroomManagementComponent } from './department/sub-modules/classroom/pages/classroom-management/classroom-management.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ScheduleManagementComponent } from './components/schedule-management/schedule-management.component';
-import { ClassroomListComponent } from './department/sub-modules/classroom/components/classroom-list/classroom-list.component';
 import { Schedule } from './components/schedule/schedule.component';
 import { ScheduleGenerationModalComponent } from './components/schedule-generation-modal/schedule-generation-modal.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ScheduleProgramComponent } from './components/schedule-program/schedule-program.component';
 
-const routes: Routes = [
-  {
-    path: 'departments/:id/classrooms',
-    component: ClassroomManagementComponent,
-  },
-  { path: 'schedules', component: Schedule },
-];
+const routes: Routes = [{ path: 'schedules', component: Schedule }];
 
 @NgModule({
   declarations: [AppComponent, ScheduleManagementComponent, Schedule, ScheduleGenerationModalComponent, HomeComponent, ScheduleProgramComponent],
--- src/app/department/sub-modules/classroom/classroom.module.ts
+++ src/app/department/sub-modules/classroom/classroom.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ClassroomListComponent } from './components/classroom-list/classroom-list.component';
 import { ClassroomManagementComponent } from './pages/classroom-management/classroom-management.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ClassroomRoutingModule } from './classroom-routing.module';
 
 @NgModule({
   declarations: [ClassroomListComponent, ClassroomManagementComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, ClassroomRoutingModule],
   exports: [ClassroomListComponent],
 })
 export class ClassroomModule {}
