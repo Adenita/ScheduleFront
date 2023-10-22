9042ce701cd3307668f210db8bc00b3a7bc5df1c
adenita <nittaluta@gmail.com>
2023-10-22 02:09:12 +0200

    created classroom routing module

---
+++ src/app/department/sub-modules/classroom/classroom-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ClassroomManagementComponent } from './pages/classroom-management/classroom-management.component';
+
+const routes: Routes = [{ path: 'departments/:id/classrooms', component: ClassroomManagementComponent }];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ClassroomRoutingModule {}
