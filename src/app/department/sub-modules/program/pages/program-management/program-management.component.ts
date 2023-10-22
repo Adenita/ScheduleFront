e4d0d218c64e46387338b9be70aea4cf307d54d2
adenita <nittaluta@gmail.com>
2023-10-22 19:29:08 +0200

    added edit/delete methods to program management component

--- src/app/department/sub-modules/program/pages/program-management/program-management.component.ts
+++ src/app/department/sub-modules/program/pages/program-management/program-management.component.ts
@@ -1,23 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ProgramService } from '../../../../../core/services/http/program.service';
 import { DepartmentService } from '../../../../../core/services/http/department.service';
 import { ActivatedRoute } from '@angular/router';
 import { RouteParametersService } from '../../../../../core/services/route-parameters.service';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subject, takeUntil } from 'rxjs';
 import { ProgramTransport, ProgramListTransport } from '../../../../../shared/models/program';
+import { DepartmentTransport } from '../../../../../shared/models/department';
 
 @Component({
   selector: 'app-program-management',
   templateUrl: './program-management.component.html',
   styleUrls: ['./program-management.component.css'],
 })
-export class ProgramManagementComponent implements OnInit {
+export class ProgramManagementComponent implements OnInit, OnDestroy {
   departmentId: number = -1;
   programs$: BehaviorSubject<ProgramTransport[]>;
   route: string = '';
   programForm: FormGroup;
   showForm = false;
+  isEditMode: boolean = false;
+  programToBeEditedId: number = -1;
+  destroyed$: Subject<void> = new Subject<void>();
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -67,11 +71,59 @@ export class ProgramManagementComponent implements OnInit {
     }
   }
 
+  deleteProgram(programId: number) {
+    this.programService
+      .delete(programId)
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe({
+        next: () => {
+          const currentPrograms: ProgramTransport[] = this.programs$.getValue();
+          const updatedPrograms: ProgramTransport[] = currentPrograms.filter((program) => program.id !== programId);
+          this.programs$.next(updatedPrograms);
+        },
+        error: (err) => console.error('Error deleting program:', err),
+      });
+  }
+
+  updateProgram(programId: number) {
+    if (this.programForm.valid) {
+      this.programService
+        .update(programId, this.programForm.value)
+        .pipe(takeUntil(this.destroyed$))
+        .subscribe({
+          next: (updatedProgram: ProgramTransport) => {
+            const currentPrograms: ProgramTransport[] = this.programs$.getValue();
+            const updatedPrograms: ProgramTransport[] = currentPrograms.map((program) => {
+              if (program.id === programId) {
+                return updatedProgram;
+              }
+              return program;
+            });
+            this.programs$.next(updatedPrograms);
+            this.closeForm();
+            this.isEditMode = false;
+          },
+          error: (err) => console.error('Error updating program:', err),
+        });
+    }
+  }
+
+  openEditForm(programId: number) {
+    this.isEditMode = true;
+    this.programToBeEditedId = programId;
+    this.openForm();
+  }
+
   openForm() {
     this.showForm = true;
   }
 
   closeForm() {
     this.showForm = false;
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
   }
 }
