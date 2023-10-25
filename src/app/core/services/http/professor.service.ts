3658dafeee865d5ceaa79f8ac2d2cf62b3407454
adenita <nittaluta@gmail.com>
2023-10-25 03:38:45 +0200

    added addsubjecttoprofessor method

--- src/app/core/services/http/professor.service.ts
+++ src/app/core/services/http/professor.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { DataService } from '../data.service';
 import { ProfessorDetailsTransport, ProfessorListTransport, ProfessorTransport } from '../../../shared/models/professor';
 import { Observable } from 'rxjs';
+import { SubjectListTransport, SubjectTransport } from '../../../shared/models/subject';
 
 @Injectable({
   providedIn: 'root',
@@ -14,5 +15,13 @@ export class ProfessorService extends DataService<ProfessorTransport, ProfessorL
   }
   getProfessorDetails(professorId: number): Observable<ProfessorDetailsTransport> {
     return this.httpClient.get<ProfessorDetailsTransport>(`${this.url}/${this.apiUrl}/${professorId}/details`);
+  }
+
+  getProfessorSubjects(professorId: number): Observable<SubjectListTransport> {
+    return this.httpClient.get<SubjectListTransport>(`${this.url}/${this.apiUrl}/${professorId}/subjects`);
+  }
+
+  addSubjectToProfessor(professorId: number, subjectId: number): Observable<SubjectTransport> {
+    return this.httpClient.post<SubjectTransport>(`${this.url}/${this.apiUrl}/${professorId}/subjects`, subjectId);
   }
 }
