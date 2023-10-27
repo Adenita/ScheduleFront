0c63d2d3a3c6e367561bb20df042064020d769ae
adenita <nittaluta@gmail.com>
2023-10-27 07:20:17 +0200

    created classroom modal management service

---
+++ src/app/features/department/features/classroom/services/classroom-modal-management.service.ts
@@ -0,0 +1,29 @@
+import { GeneralData, ModalManagementService } from '../../../shared/services/modal-management.service';
+import { ClassroomFormModalComponent } from '../components/classroom-form-modal/classroom-form-modal.component';
+import { FormGroup } from '@angular/forms';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Classroom } from '../../../../../shared/models/classroom';
+
+export interface ClassroomModalData extends GeneralData {}
+@Injectable({
+  providedIn: 'root',
+})
+export class ClassroomModalManagementService extends ModalManagementService<
+  ClassroomFormModalComponent,
+  ClassroomModalData
+> {
+  bindClassroomModalData(
+    selectedClassroomId: number,
+    classroomForm: FormGroup,
+    isEditMode: boolean,
+    classrooms$: BehaviorSubject<Classroom[]>,
+  ): ClassroomModalData {
+    return {
+      selectedId: selectedClassroomId,
+      form: classroomForm,
+      data$: classrooms$,
+      isEditMode: isEditMode,
+    };
+  }
+}
