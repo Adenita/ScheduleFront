af1e79ce87e25540d56561df18c6435d387075ce
adenita <nittaluta@gmail.com>
2023-10-27 07:50:33 +0200

    added form to module

--- src/app/features/department/features/student-group/student-group.module.ts
+++ src/app/features/department/features/student-group/student-group.module.ts
@@ -4,9 +4,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { StudentGroupListComponent } from './components/student-group-list/student-group-list.component';
 import { StudentGroupManagementComponent } from './pages/student-group-management/student-group-management.component';
 import { StudentGroupRoutingModule } from './student-group-routing.module';
+import { StudentGroupFormModalComponent } from './components/student-group-form-modal/student-group-form-modal.component';
 
 @NgModule({
-  declarations: [StudentGroupListComponent, StudentGroupManagementComponent],
+  declarations: [StudentGroupListComponent, StudentGroupManagementComponent, StudentGroupFormModalComponent],
   imports: [CommonModule, ReactiveFormsModule, StudentGroupRoutingModule],
   exports: [StudentGroupListComponent],
 })
