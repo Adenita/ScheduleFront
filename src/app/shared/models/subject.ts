0b280c5b6eb9a149751fef4533ba30b5737201aa
adenita <nittaluta@gmail.com>
2023-10-27 17:11:18 +0200

    added requiremntType and hours to subject

--- src/app/shared/models/subject.ts
+++ src/app/shared/models/subject.ts
@@ -7,12 +7,31 @@ export enum LabRequirement {
   EXERCISE_ONLY = 'Exercise only',
 }
 
+export enum RequirementType {
+  MANDATORY = 'Mandatory',
+  OPTIONAL = 'Optional',
+}
+
+export enum Hours {
+  ONE_TWO = '1+2',
+  ONE_THREE = '1+3',
+  TWO_ONE = '2+1',
+  TWO_TWO = '2+2',
+  TWO_THREE = '2+3',
+  TWO_FOUR = '2+4',
+  THREE_ONE = '3+1',
+  THREE_TWO = '3+2',
+  THREE_THREE = '3+3',
+}
+
 export interface SubjectTransport {
   id: number;
   name: string;
   etcs: number;
   requiresLab: LabRequirement;
   semester: number;
+  requirementType: RequirementType;
+  hours: Hours;
 }
 
 export interface SubjectDetailsTransport {
@@ -21,6 +40,8 @@ export interface SubjectDetailsTransport {
   etcs: number;
   requiresLab: LabRequirement;
   semester: number;
+  requirementType: RequirementType;
+  hours: Hours;
   professors: ProfessorTransport[];
   studentGroups: StudentGroupTransport[];
 }
