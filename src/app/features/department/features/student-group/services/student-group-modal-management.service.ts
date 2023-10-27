11b3dbcf7b5913e379bbe3f16c975270c7e0bb69
adenita <nittaluta@gmail.com>
2023-10-27 07:50:51 +0200

    created student group modal management service

---
+++ src/app/features/department/features/student-group/services/student-group-modal-management.service.ts
@@ -0,0 +1,33 @@
+import { GeneralModalData, ModalManagementService } from '../../../shared/services/modal-management.service';
+import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+import { StudentGroupFormModalComponent } from '../components/student-group-form-modal/student-group-form-modal.component';
+import { GroupType, StudentGroupTransport } from '../../../../../shared/models/student-group';
+
+export interface StudentGroupModalData extends GeneralModalData {
+  groupTypes: GroupType[];
+}
+@Injectable({
+  providedIn: 'root',
+})
+export class StudentGroupModalManagementService extends ModalManagementService<
+  StudentGroupFormModalComponent,
+  StudentGroupModalData
+> {
+  bindStudentGroupModalData(
+    selectedStudentGroupId: number,
+    classroomForm: FormGroup,
+    isEditMode: boolean,
+    studentGroups$: BehaviorSubject<StudentGroupTransport[]>,
+    groupTypes: GroupType[],
+  ): StudentGroupModalData {
+    return {
+      selectedId: selectedStudentGroupId,
+      form: classroomForm,
+      data$: studentGroups$,
+      isEditMode: isEditMode,
+      groupTypes,
+    };
+  }
+}
