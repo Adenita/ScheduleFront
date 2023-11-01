d94b9371ef82dc1cce6eeb0690e45f66f7c33fec
adenita <nittaluta@gmail.com>
2023-11-01 03:11:39 +0100

    refactored department models to match backend model changes

--- src/app/shared/models/department.ts
+++ src/app/shared/models/department.ts
@@ -1,22 +1,32 @@
-import { ProgramDetailsTransport } from './program';
+import { ProgramSubjectDetailsTransport, ProgramTransport } from './program';
 import { ProfessorTransport } from './professor';
 import { Timeslot } from './timeslots';
 import { Classroom } from './classroom';
 import { SubjectTransport } from './subject';
+import { StudentGroupTransport } from './student-group';
 
 export interface DepartmentTransport {
   id: number;
   name: string;
 }
 
-export interface DepartmentDetailTransport {
+export interface DepartmentScheduleDetailTransport {
   id: number;
   name: string;
-  programTransports: ProgramDetailsTransport[];
-  subjectTransports: SubjectTransport[];
+  programTransports: ProgramSubjectDetailsTransport[];
   classrooms: Classroom[];
   professorTransports: ProfessorTransport[];
   timeslots: Timeslot[];
+}
+
+export interface DepartmentDetailTransport {
+  id: number;
+  name: string;
+  programTransports: ProgramTransport[];
+  subjectTransports: SubjectTransport[];
+  studentGroupTransports: StudentGroupTransport[];
+  classrooms: Classroom[];
+  professorTransports: ProfessorTransport[];
 }
 
 export interface DepartmentListTransport {
