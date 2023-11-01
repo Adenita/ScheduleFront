313f16ebb358f057079f243a039bd1a17adbf23c
adenita <nittaluta@gmail.com>
2023-11-01 03:11:03 +0100

    refactored program models to match backend model changes

--- src/app/shared/models/program.ts
+++ src/app/shared/models/program.ts
@@ -1,16 +1,24 @@
 import { StudentGroupTransport } from './student-group';
-import { SubjectDetailsTransport } from './subject';
+import { SubjectDetailsTransport, SubjectTransport } from './subject';
+import { ProfessorTransport } from './professor';
 
 export interface ProgramTransport {
   id: number;
   name: string;
 }
 
-export interface ProgramDetailsTransport {
+export interface ProgramSubjectDetailsTransport {
   id: number;
   name: string;
   subjectsTransport: SubjectDetailsTransport[];
+}
+
+export interface ProgramDetailsTransport {
+  id: number;
+  name: string;
+  subjectTransports: SubjectTransport[];
   studentGroupTransports: StudentGroupTransport[];
+  professorTransports: ProfessorTransport[];
 }
 
 export interface ProgramListTransport {
