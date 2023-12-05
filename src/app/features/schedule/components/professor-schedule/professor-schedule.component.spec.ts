8f82b014b93dcde0a9d7f31198af39db9c0dd329
adenita <nittaluta@gmail.com>
2023-12-05 08:06:17 +0100

    added professor-schedule component

---
+++ src/app/features/schedule/components/professor-schedule/professor-schedule.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfessorScheduleComponent } from './professor-schedule.component';
+
+describe('ProfessorScheduleComponent', () => {
+  let component: ProfessorScheduleComponent;
+  let fixture: ComponentFixture<ProfessorScheduleComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ProfessorScheduleComponent]
+    });
+    fixture = TestBed.createComponent(ProfessorScheduleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
