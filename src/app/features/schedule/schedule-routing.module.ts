47805834bf987936cf54eaddf54a64a6a6cae982
adenita <nittaluta@gmail.com>
2023-12-05 02:53:47 +0100

    added routes for new schedule component

--- src/app/features/schedule/schedule-routing.module.ts
+++ src/app/features/schedule/schedule-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ScheduleManagementComponent } from './pages/schedule-management/schedule-management.component';
 import { ClassroomScheduleManagementComponent } from './pages/classroom-schedule-management/classroom-schedule-management.component';
+import { ProgramScheduleManagementComponent } from './pages/program-schedule-management/program-schedule-management.component';
 
 const routes: Routes = [
   { path: 'schedules', component: ScheduleManagementComponent },
   { path: 'departments/:id/schedules', component: ScheduleManagementComponent },
   { path: 'schedules/:scid/classrooms/:cid', component: ClassroomScheduleManagementComponent },
   { path: 'departments/:id/schedules/:scid/classrooms/:cid', component: ClassroomScheduleManagementComponent },
+  { path: 'schedules/:scid/programs/:pid', component: ProgramScheduleManagementComponent },
+  { path: 'departments/:id/schedules/:scid/programs/:pid', component: ProgramScheduleManagementComponent },
 ];
 
 @NgModule({
