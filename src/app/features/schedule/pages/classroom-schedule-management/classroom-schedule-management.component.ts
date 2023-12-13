d726ab68741d2b0130623a8873d240834921c9f8
adenita <nittaluta@gmail.com>
2023-12-13 13:18:53 +0100

    added goBack button and method to navigate to main schedule page for classroom schedule page

--- src/app/features/schedule/pages/classroom-schedule-management/classroom-schedule-management.component.ts
+++ src/app/features/schedule/pages/classroom-schedule-management/classroom-schedule-management.component.ts
@@ -96,6 +96,14 @@ export class ClassroomScheduleManagementComponent implements OnInit, OnDestroy {
       });
   }
 
+  goBack() {
+    if (this.departmentId != -1) {
+      this.router.navigate(['departments', this.departmentId, 'schedules']);
+    } else {
+      this.router.navigate(['schedules']);
+    }
+  }
+
   ngOnDestroy(): void {
     this.destroyed$.next();
     this.destroyed$.complete();
