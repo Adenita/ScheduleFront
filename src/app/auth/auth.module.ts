31e122eb6530e4713d9f9cdeb7f1bf77df454f48
adenita <nittaluta@gmail.com>
2024-02-12 06:45:34 +0100

    added interceptor service

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormModalComponent } from './components/login-form-modal/login-form-modal.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { InterceptorService } from './services/interceptor.service';
 
 @NgModule({
   declarations: [LoginFormModalComponent],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: InterceptorService, multi: true }],
   imports: [CommonModule, ReactiveFormsModule],
 })
 export class AuthModule {}
