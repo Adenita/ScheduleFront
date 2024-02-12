e3d64025efeaae21824e905f13c33546a09464b7
adenita <nittaluta@gmail.com>
2024-02-12 14:56:42 +0100

    added authguard to routing modules

--- src/app/features/department/features/professor/professor-routing.module.ts
+++ src/app/features/department/features/professor/professor-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfessorManagementComponent } from './pages/professor-management/professor-management.component';
 import { ProfessorDetailsComponent } from './pages/professor-details/professor-details.component';
+import { AuthGuard } from '../../../../auth/auth.guard';
+import { Role } from '../../../../shared/models/user';
 
 const routes: Routes = [
   {
@@ -11,6 +13,8 @@ const routes: Routes = [
   {
     path: 'departments/:id/professors/:ppid',
     component: ProfessorDetailsComponent,
+    canActivate: [AuthGuard],
+    data: { role: [Role.ADMIN, Role.PROFESSOR] },
   },
   {
     path: 'departments/:id/programs/:pid/professors',
--- src/app/features/department/features/user/user-routing.module.ts
+++ src/app/features/department/features/user/user-routing.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserManagementComponent } from './pages/user-management/user-management.component';
 import { UserDetailsComponent } from './pages/user-details/user-details.component';
+import { AuthGuard } from '../../../../auth/auth.guard';
+import { Role } from '../../../../shared/models/user';
 
 const routes: Routes = [
   {
     path: 'departments/:id/users',
     component: UserManagementComponent,
+    canActivate: [AuthGuard],
+    data: { role: [Role.ADMIN] },
   },
   {
     path: 'profile',
     component: UserDetailsComponent,
+    canActivate: [AuthGuard],
+    data: { role: [Role.ADMIN, Role.PROFESSOR] },
   },
 ];
 
