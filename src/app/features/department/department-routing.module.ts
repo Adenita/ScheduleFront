739b61f9f1f0259eed6bccd9c7123541a48147f7
adenita <nittaluta@gmail.com>
2024-02-24 04:21:56 +0100

    lazily loaded modules into department module

--- src/app/features/department/department-routing.module.ts
+++ src/app/features/department/department-routing.module.ts
@@ -2,10 +2,45 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DepartmentDetailsComponent } from './pages/department-details/department-details.component';
 import { DepartmentsListComponent } from './components/departments-list/departments-list.component';
+import { UserDetailsComponent } from './features/user/pages/user-details/user-details.component';
 
 const routes: Routes = [
-  { path: 'departments', component: DepartmentsListComponent },
-  { path: 'departments/:id', component: DepartmentDetailsComponent },
+  { path: '', component: DepartmentsListComponent },
+  {
+    path: ':id',
+    component: DepartmentDetailsComponent,
+    children: [
+      {
+        path: 'programs',
+        loadChildren: () => import('./features/program/program.module').then((m) => m.ProgramModule),
+      },
+      {
+        path: 'professors',
+        loadChildren: () => import('./features/professor/professor.module').then((m) => m.ProfessorModule),
+      },
+      {
+        path: 'subjects',
+        loadChildren: () => import('./features/subject/subject.module').then((m) => m.SubjectModule),
+      },
+      {
+        path: 'classrooms',
+        loadChildren: () => import('./features/classroom/classroom.module').then((m) => m.ClassroomModule),
+      },
+      {
+        path: 'users',
+        loadChildren: () => import('./features/user/user.module').then((m) => m.UserModule),
+      },
+      {
+        path: 'student-groups',
+        loadChildren: () => import('./features/student-group/student-group.module').then((m) => m.StudentGroupModule),
+      },
+    ],
+  },
+
+  {
+    path: 'profile',
+    component: UserDetailsComponent,
+  },
 ];
 
 @NgModule({
