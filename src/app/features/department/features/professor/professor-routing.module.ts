6338cfd8ee0caf6f7d2c3afe69e88577c4ce472b
adenita <nittaluta@gmail.com>
2024-02-24 04:37:43 +0100

    refactored professors routing module

--- src/app/features/department/features/professor/professor-routing.module.ts
+++ src/app/features/department/features/professor/professor-routing.module.ts
@@ -2,43 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfessorManagementComponent } from './pages/professor-management/professor-management.component';
 import { ProfessorDetailsComponent } from './pages/professor-details/professor-details.component';
-import { AuthGuard } from '../../../../auth/auth.guard';
-import { Role } from '../../../../shared/models/user';
 
 const routes: Routes = [
   {
-    path: 'departments/:id/professors',
+    path: '',
     component: ProfessorManagementComponent,
-  },
-  {
-    path: 'departments/:id/professors/:ppid',
-    component: ProfessorDetailsComponent,
-    canActivate: [AuthGuard],
-    data: { role: [Role.ADMIN, Role.PROFESSOR] },
-  },
-  {
-    path: 'departments/:id/programs/:pid/professors',
-    component: ProfessorManagementComponent,
-  },
-  {
-    path: 'departments/:id/programs/:pid/professors/:ppid',
-    component: ProfessorDetailsComponent,
-  },
-  {
-    path: 'departments/:id/subjects/:sid/professors',
-    component: ProfessorManagementComponent,
-  },
-  {
-    path: 'departments/:id/subjects/:sid/professors/:ppid',
-    component: ProfessorDetailsComponent,
-  },
-  {
-    path: 'departments/:id/programs/:pid/subjects/:sid/professors',
-    component: ProfessorManagementComponent,
-  },
-  {
-    path: 'departments/:id/programs/:pid//subjects/:sid/professors/:ppid',
-    component: ProfessorDetailsComponent,
+    children: [
+      {
+        path: ':ppid',
+        component: ProfessorDetailsComponent,
+      },
+    ],
   },
 ];
 
