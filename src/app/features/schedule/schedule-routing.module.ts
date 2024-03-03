a611e87a48521c0a23cf991756e51830631a6e4d
adenita <nittaluta@gmail.com>
2024-03-03 17:24:15 +0100

    refactored routing module

--- src/app/features/schedule/schedule-routing.module.ts
+++ src/app/features/schedule/schedule-routing.module.ts
@@ -4,16 +4,14 @@ import { ScheduleManagementComponent } from './pages/schedule-management/schedul
 import { ClassroomScheduleManagementComponent } from './pages/classroom-schedule-management/classroom-schedule-management.component';
 import { ProgramScheduleManagementComponent } from './pages/program-schedule-management/program-schedule-management.component';
 import { ProfessorScheduleManagementComponent } from './pages/professor-schedule-management/professor-schedule-management.component';
+// import { ScheduleGeneratorComponent } from './pages/schedule-generator/schedule-generator.component';
 
 const routes: Routes = [
-  { path: 'schedules', component: ScheduleManagementComponent },
-  { path: 'departments/:id/schedules', component: ScheduleManagementComponent },
-  { path: 'schedules/:scid/classrooms/:cid', component: ClassroomScheduleManagementComponent },
-  { path: 'departments/:id/schedules/:scid/classrooms/:cid', component: ClassroomScheduleManagementComponent },
-  { path: 'schedules/:scid/programs/:pid', component: ProgramScheduleManagementComponent },
-  { path: 'departments/:id/schedules/:scid/programs/:pid', component: ProgramScheduleManagementComponent },
-  { path: 'schedules/:scid/professors/:ppid', component: ProfessorScheduleManagementComponent },
-  { path: 'departments/:id/schedules/:scid/professors/:ppid', component: ProfessorScheduleManagementComponent },
+  { path: '', component: ScheduleManagementComponent },
+  { path: ':scid/classrooms/:cid', component: ClassroomScheduleManagementComponent },
+  { path: ':scid/programs/:pid', component: ProgramScheduleManagementComponent },
+  { path: ':scid/professors/:ppid', component: ProfessorScheduleManagementComponent },
+  // { path: 'generate', component: ScheduleGeneratorComponent },
 ];
 
 @NgModule({
