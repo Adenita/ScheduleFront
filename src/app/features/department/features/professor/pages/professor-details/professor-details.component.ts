08409878896bdb33b874f76a540b111c926108fc
adenita <nittaluta@gmail.com>
2024-04-08 08:24:19 +0200

    added preferred day as tab and checked for permission

--- src/app/features/department/features/professor/pages/professor-details/professor-details.component.ts
+++ src/app/features/department/features/professor/pages/professor-details/professor-details.component.ts
@@ -3,6 +3,8 @@ import { BehaviorSubject, Subject, takeUntil } from 'rxjs';
 import { RouteParametersService } from '../../../../../../core/services/route-parameters.service';
 import { ProfessorDetailsTransport } from '../../../../../../shared/models/professor';
 import { ProfessorService } from '../../../../../../core/services/http/professor.service';
+import { Role } from '../../../../../../shared/models/user';
+import { PermissionService } from '../../../../../../auth/services/permission.service';
 
 @Component({
   selector: 'app-professor-details',
@@ -14,10 +16,12 @@ export class ProfessorDetailsComponent implements OnInit, OnDestroy {
   professor: ProfessorDetailsTransport = {} as ProfessorDetailsTransport;
   currentRoute: string = '';
   destroyed$: Subject<void> = new Subject<void>();
+  hasPermission: boolean = false;
 
   constructor(
     private routeParametersService: RouteParametersService,
     private professorService: ProfessorService,
+    private permissionService: PermissionService,
   ) {}
 
   ngOnInit(): void {
@@ -25,6 +29,7 @@ export class ProfessorDetailsComponent implements OnInit, OnDestroy {
       this.professorId = this.routeParametersService.professorId;
       this.getProfessor(this.professorId);
     });
+    this.hasPermission = this.permissionService.hasAnyRole([Role.ADMIN, Role.PROFESSOR]);
   }
 
   getProfessor(professorId: number) {
