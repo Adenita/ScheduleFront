4ebd98bfeca5ca065fcbfd651576b573c3cc8f1b
adenita <nittaluta@gmail.com>
2024-04-09 08:55:41 +0200

    added schedule generator management component

---
+++ src/app/features/schedule/pages/schedule-generator-management/schedule-generator-management.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DepartmentListTransport, DepartmentTransport } from '../../../../shared/models/department';
+import { Subject, takeUntil } from 'rxjs';
+
+import { ActivatedRoute } from '@angular/router';
+import { DepartmentService } from '../../../../core/services/http/department.service';
+
+@Component({
+  selector: 'app-schedule-generator-management',
+  templateUrl: './schedule-generator-management.component.html',
+  styleUrls: ['./schedule-generator-management.component.scss'],
+})
+export class ScheduleGeneratorManagementComponent implements OnInit, OnDestroy {
+  departments: DepartmentTransport[] = [];
+
+  currentRoute: string = '';
+  destroyed$: Subject<void> = new Subject<void>();
+
+  constructor(
+    protected activatedRoute: ActivatedRoute,
+    private departmentService: DepartmentService,
+  ) {}
+
+  ngOnInit() {
+    this.getDepartments();
+  }
+
+  getDepartments() {
+    this.departmentService
+      .getAll()
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe({
+        next: (departmentListTransport: DepartmentListTransport) => {
+          this.departments = departmentListTransport.departmentTransportList;
+        },
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
+}
--- src/app/features/schedule/schedule.module.ts
+++ src/app/features/schedule/schedule.module.ts
@@ -10,8 +10,9 @@ import { ClassroomScheduleManagementComponent } from './pages/classroom-schedule
 import { ProgramScheduleManagementComponent } from './pages/program-schedule-management/program-schedule-management.component';
 import { ProfessorScheduleManagementComponent } from './pages/professor-schedule-management/professor-schedule-management.component';
 import { ProfessorScheduleComponent } from './components/professor-schedule/professor-schedule.component';
-import { ScheduleGeneratorComponent } from './pages/schedule-generator/schedule-generator.component';
+import { ScheduleGeneratorComponent } from './components/schedule-generator/schedule-generator.component';
 import { ScheduleCalendarComponent } from './components/schedule-calendar/schedule-calendar.component';
+import { ScheduleGeneratorManagementComponent } from './pages/schedule-generator-management/schedule-generator-management.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { ScheduleCalendarComponent } from './components/schedule-calendar/schedu
     ProfessorScheduleComponent,
     ScheduleGeneratorComponent,
     ScheduleCalendarComponent,
+    ScheduleGeneratorManagementComponent,
   ],
   imports: [CommonModule, ScheduleRoutingModule],
   exports: [ScheduleManagementComponent],
